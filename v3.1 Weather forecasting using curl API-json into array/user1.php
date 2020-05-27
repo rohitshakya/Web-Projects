778f838d73575e7830072650f25ee68578b4c33c
Rohit Shakya <rohit.rkshakya@gmail.com>
2020-05-28 01:21:42 +0530

    v3.1 Weather forecasting using curl API-json into array

---
+++ v3.1 Weather forecasting using curl API-json into array/user1.php
@@ -0,0 +1,102 @@
+<!--
+ * Author : Rohit Shakya
+ * Date   : May-2020
+ * Editor : Sublime text
+ * Local server: Xampp
+ * Title : Username and Password Authentication System 
+ -->
+<?php
+session_start(); 
+?> 
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+	<title>UserPage</title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  </head>
+<body style="background: white">
+<!-- nav bar-->
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Weather</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href=logout.php><span class="glyphicon glyphicon-log-in"></span> Log Out</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<!--nav bar complete-->
+<strong><h4>Welcome
+<?php
+if(!isset($_SESSION['username']))
+{header('Location: home1.html');}
+else
+{echo ucfirst($_SESSION['username']); } ?>!!</h4></strong>
+
+<?php
+$apiKey = "231a533e913c7e004f7ea56e36a67d83";
+$cityId = 1261481;
+$googleApiUrl = "http://api.openweathermap.org/data/2.5/weather?id=" . $cityId . "&lang=en&units=metric&APPID=" . $apiKey;
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_URL, $googleApiUrl);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_VERBOSE, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$response = curl_exec($ch);
+
+curl_close($ch);
+$data = json_decode($response);
+$currentTime = time();
+?>
+    <div class="report-container">
+        <h3><?php echo $data->name; ?> Weather Status</h3>
+        <div class="time">
+          <?php date_default_timezone_set('Asia/Kolkata'); ?>
+            <div><?php echo date("l g:i a", $currentTime); ?></div>
+            <div><?php echo date("jS F, Y",$currentTime); ?></div>
+            <div><?php echo ucwords($data->weather[0]->description); ?></div>
+
+        </div>
+        <div class="weather-forecast">
+            <img
+                src="http://openweathermap.org/img/w/<?php echo $data->weather[0]->icon; ?>.png"
+                class="weather-icon" /> <?php echo $data->main->temp_max; ?>°C<span
+                class="min-temperature"><?php echo $data->main->temp_min; ?>°C</span>
+        </div>
+        <div class="time">
+            <div>Humidity: <?php echo $data->main->humidity; ?> %</div>
+            <div>Wind: <?php echo $data->wind->speed; ?> km/h</div>
+        </div>
+    </div>
+    
+<!--accessing json file into php page, converting json data in php array-->
+<?php 
+
+//$str = file_get_contents('http://bulk.openweathermap.org/sample/city.list.json.gz');
+ $str = file_get_contents('city/city.json');
+ $json = json_decode($str, true); // decode the JSON into an associative array
+// echo '<pre>' . print_r($json, true) . '</pre>'; //printing whole json file in form of json
+ echo $cityName = $json[0]['name'];
+?>
+</body>
+</html>
