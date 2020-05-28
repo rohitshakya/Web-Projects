35e8c7da628c8ffbcb1362cc50878b36c4e3c42a
Rohit Shakya <rohit.rkshakya@gmail.com>
2020-05-28 13:15:15 +0530

    Searchbox

---
+++ v3.1.2 Auto Complete Searchbox javascipt/searchphp.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  <title>HomePage</title>
+  <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
+<body>
+
+<h1><?php
+ echo  $_GET['myCountry'];
+?></h1>
+
+</body>
+</html>
