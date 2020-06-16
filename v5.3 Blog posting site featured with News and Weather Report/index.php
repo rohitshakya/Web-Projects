f4f945b582f87e3d2b008ab29775a71ccf201a14
Rohit Shakya <rohit.rkshakya@gmail.com>
2020-06-16 20:24:56 +0530

    Create index.php

---
+++ v5.3 Blog posting site featured with News and Weather Report/index.php
@@ -0,0 +1,11 @@
+<!--
+ * Author : Rohit Shakya
+ * Date   : June-2020
+ * Editor : Sublime text
+ * Local server: Xampp
+ * Title : Blog posting site featuring Weather, News and Geolocation report  
+ * Version: v5.3
+ -->
+ <?php 
+  	header('Location:home1.html');
+ ?>
