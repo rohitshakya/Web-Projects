3c4ea06585f082be147579b0f317551e917acdee
Rohit Shakya <rohit.rkshakya@gmail.com>
2021-02-28 14:17:53 +0530

    update

--- v9.0 Blood Bank/bloodbank/pages/logout.php
+++
@@ -1,7 +0,0 @@
-
- <?php
- //page for destroying all the session variables and session itself
- session_start();
- session_destroy();
- header('Location: ../index.html'); //redirection to index.html
-?>
